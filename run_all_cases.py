f342133571289a7603386c96330e9adca6748883
RyanDavies19 <ryandavies2019@gmail.com>
2023-10-06 11:14:02 -0600

    Removed moorpy dependency (except for helpers)

--- run_all_cases.py
+++ run_all_cases.py
@@ -4,33 +4,37 @@ import os
 if __name__ == "__main__":
 
     # Flags for running
-    versions = {'run_v1' : False, 'run_dev2' : False, 'run_cpy' : True, 'run_c' : False, 'run_f' : True}
+    versions = {'run_v1' : False, 'run_dev2' : False, 'run_cpy' : False, 'run_c' : True, 'run_f' : True}
     
+    # Required
     dynamics_args = {'static' : True, 
                      'sin' : False, 
                      'from_file' : False, 
+                     # if sin is true
                      'period' : 10, 
-                     'A' : 10, # Amplitude of driving funtion
-                     'axis' : 0} # Axis of oscillation x: 0, y: 1, z: 2
+                     'A' : 6, 
+                     'axis' : 5 # 0 -> x, 1 -> y, 2 -> z, 3 -> rx , 4 -> ry, 5 -> rz
+                     }
 
-
+    # Required
     run_args = {'debug' : False, 
                 'simulate' : True,
                 'plot' : True,
                 'rootname' : '' ,
                 'extension' : '', 
                 'path' : 'MooringTest/', 
-                'tMax' : 10.0,  # simulation duration (s)
-                'dof' : 3} # Size of X and XD vectors: 3 DOF for lines, points, connections, 6 DOF for bodies and rods. Ex for three points, size should be 9. 
+                'tMax' : 20,  # simulation duration (s)
+                'dof' : 3} # DOF of coupled objects: 3 DOF for lines, points, connections, 6 DOF for bodies and rods (for no coupled objects, set to 3). TODO: make this work for coupled bodies and rods at the same time
 
+    # Required if plot is true in run_args
     plot_args = {}
-    if run_args['plot']:
-        plot_args = {'display': True, 
+    if run_args['plot']: # for plotting moordyn outputs
+        plot_args = {'display': False, 
                      'save': True,    
                      'line_rmse': False, 
                      'ten_rmse': False, 
                      'plot_channels': True,
-                     'lines_and_tens': False,
+                     'lines_and_tens': False, 
                      'animate_all': False,
                      'animate_start_end': False,
                      'plot_individual_start_end': False,
@@ -39,7 +43,9 @@ if __name__ == "__main__":
                      'plot2d': False,
                      'from_saved_runs': False,
                      'outputs_dir' : None,
-                     'plot_tRange': 'All'}
+                     'v1' : False,
+                     'one_dataset' : False,
+                     'plot_tRange': 'All'} # needs to be double
 
     # Read in a list of files in MooringTest
     os.system('OS_scripts/clean_outputs')
@@ -50,8 +56,8 @@ if __name__ == "__main__":
     for in_file in file_list:
         if ("dev2" in in_file) or ("Cpy" in in_file) or ("C" in in_file) or ("_mod" in in_file) or ("motions" in in_file):
             file_list.remove(in_file)
-        if ("cable" in in_file):
-            file_list.remove(in_file)
+        # if ("cable" in in_file):
+        #     file_list.remove(in_file)
     # Notes about inputs files:
     #   case4.dat and lines.txt are 3d systems
     #   v1 calculates incorrect tensions for case4.dat () 
